6e50eb0346ca592218983b4d5abf96ab5d04a272
= <ingjohnson7@gmail.com>
2018-02-28 00:56:52 -0400

    adding firebase

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
-var vFire = new Vue({
-    el: '.container',
-    data: {
-        newItem:'',
-        editingItem: null,
-        items: [
-            {name: 'To buy a cat', completed: false},
-            {name: 'Eat a pizza', completed: false},
-            {name: 'To buy a dog', completed: true}
-        ]
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyADHMik9SVhJcHjf5csPPzryHOQ6-GzLc0",
+    authDomain: "vuefireitems.firebaseapp.com",
+    databaseURL: "https://vuefireitems.firebaseio.com",
+    projectId: "vuefireitems",
+    storageBucket: "",
+    messagingSenderId: "394162984324"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+Vue.component('item-template', {
+
+    template: '#item-template',
+
+    data: function() {
+        return {
+            newItem:'',
+            editingItem: null,
+        };
     },
+
+    props: ['items'],
+    
     methods: {
         addNewItem: function() {
-            if(this.newItem.length > 3){
-                this.items.unshift({
-                    name: this.newItem,
-                    completed: false
-                });
-                this.newItem = '';
-            }
+            db.ref('items/').push({
+                name: this.newItem,
+                completed: false
+            });
+            this.newItem = '';
         },
 
         editItem: function(item) {            
-            console.info(item);
+            db.ref('items/' + item['.key']).update({
+                name: item.name
+            });
+        },
+
+        updateState: function(state, item) {
+            db.ref('items/' + item['.key']).update({
+                completed: state ? true : false
+            });
         },
         
-        removeItem: function(index) {
-            this.items.splice(index, 1);
+        removeItem: function(item) {
+            db.ref('items/' + item['.key']).remove();
         }
     }
+})
+
+var vFire = new Vue({
+    el: '.container',
+    data: {
+        items: []
+    },
+    mounted: function() {
+        db.ref('items/').on('value', function(snapshot){
+            vFire.items = [];
+            var obj = snapshot.val();
+            for(var prop in obj) {
+                vFire.items.unshift({
+                    '.key': prop,
+                    completed: obj[prop].completed,
+                    name: obj[prop].name
+                });
+            }
+        });
+    }  
 });    
